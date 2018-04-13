c6e5927fc17828242b498be588cdad5f0bcde346
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-04-13 15:32:56 +0900

    딕셔너리 초기화

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -6,14 +6,40 @@ using System.Threading.Tasks;
 
 namespace _7._1_Dictionary
 {
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Code { get; set; }
+
+        public Employee(int code, string name)
+        {
+            this.Code = code;
+            this.Name = name;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var flowerDict = new Dictionary<string, int>();
+            var flowerDict = new Dictionary<string, int>
+            {
 
-            // 딕셔너리 키 추가
-            flowerDict["violet"] = 600;
+                // 딕셔너리 키 추가
+                ["sumflower"] = 400,
+                ["pansy"] = 300,
+                ["rulip"] = 350,
+                ["rose"] = 500,
+                ["dahlia"] = 450,
+            };
+            var employeeDict = new Dictionary<int, Employee>
+            {
+                [100] = new Employee(100, "이몽룡"),
+                [112] = new Employee(112, "변학도"),
+                [125] = new Employee(125, "성춘향"),
+
+            };
+            
+            
 
             // 딕셔너리 값 출력
             Console.WriteLine(flowerDict["violet"]);
