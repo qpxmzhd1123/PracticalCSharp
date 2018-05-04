3d0733158dd5aa0d21bba4161ce0391d1f120a38
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-05-04 18:14:55 +0900

    Abbreviations 클래스 만들기

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -1,51 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _7._1_Dictionary
 {
-    class Acount
+   class Abbrreviations
     {
-        public string Id { get; set; }
-        public string Pw { get; set; }
+        private Dictionary<string, string> _dict = new Dictionary<string, string>();
 
-        public override bool Equals(object obj)
+        //생성자
+        public Abbrreviations()
         {
-            var acount = obj as Acount;
-            return acount != null &&
-                   Id == acount.Id;
-                  
+            var lines = File.ReadAllLines("Abbreviations.txt");
+            _dict = lines.Select(line => line.Split('=')).ToDictionary(x => x[0], x => x[1]);
         }
 
-        public override int GetHashCode()
+        //요소를 추가한다
+        public void Add(string abbrr, string korean)
         {
-            var hashCode = -676140440;
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Id);
-            return hashCode;
+            _dict[abbrr] = korean;
+        }
+
+        // 인덱서: 줄임말을 키로 사용한다
+        public string this[string abbr]
+        {
+            get { return _dict.ContainsKey(abbr) ? _dict[abbr] : null; }
+        }
+
+        // 한국어로부터 그에 대응하는 줄임말을 구한다
+        public string ToAbbreviation(string korean)
+        {
+            return _dict.FirstOrDefault(x => x.Value == korean).Key;
+        }
+
+        // 한국어의 위치를 인수에 넘겨주고 그것이 포함되는 요소(key, value)를 모두 구한다
+        public IEnumerable<KeyValuePair<string, string>> FindAll(string substring)
+        {
+            foreach (var item in _dict)
+            {
+                if (item.Value.Contains(substring))
+                {
+                    yield return item;
+                }
+            }
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            HashSet<Acount> set = new HashSet<Acount>()
-            {
-                new Acount{Id = "abcd", Pw = "abcd"},
-                new Acount{Id = "abcd", Pw = "abcd"},
-                new Acount{Id = "abcd2", Pw = "abcd"},
-                new Acount{Id = "abcd3", Pw = "abcd"},
-                new Acount{Id = "abcd4", Pw = "abcd"},
-                new Acount{Id = "abcd5", Pw = "abcd"},
-            };
-
-
-
-            foreach (var item in set)
-            {
-                Console.WriteLine($"ID는 {item.Id} PW는 {item.Pw}");
-            }
+            
         }
     }
 }
