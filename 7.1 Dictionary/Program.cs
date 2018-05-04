8ed24441ec0165c4db987793621c09f7693ef897
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-05-04 16:45:40 +0900

    딕셔너리 필터링 공부

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -6,57 +6,32 @@ using System.Threading.Tasks;
 
 namespace _7._1_Dictionary
 {
-    class Employee
+   class Acount
     {
-        public string Name { get; set; }
-        public int Code { get; set; }
-
-        public Employee(int code, string name)
-        {
-            this.Code = code;
-            this.Name = name;
-        }
+        public string Id { get; set; }
+        public string Pw { get; set; }
+        public DateTime JoinDate { get; set; }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Aa");
-            var flowerDict = new Dictionary<string, int>
+            Dictionary<string, Acount> userDict = new Dictionary<string, Acount>()
             {
-                // 딕셔너리 키 추가
-                ["sumflower"] = 400,
-                ["pansy"] = 300,
-                ["rulip"] = 350,
-                ["rose"] = 500,
-                ["dahlia"] = 450,
-            };
-            var employeeDict = new Dictionary<int, Employee>
-            {
-                [100] = new Employee(100, "이몽룡"),
-                [112] = new Employee(112, "변학도"),
-                [125] = new Employee(125, "성춘향"),
+                ["abcd"] = new Acount() { Id = "abcd", Pw = "abcd", JoinDate = new DateTime(2018, 12, 30) },
+                ["efgh"] = new Acount() { Id = "efgh", Pw = "efgh", JoinDate = new DateTime(2018, 11, 23) },
+                ["ijklm"] = new Acount() { Id = "ijklm", Pw = "ijklm", JoinDate = new DateTime(2017, 11, 13) },
+                ["nopq"] = new Acount() { Id = "nopq", Pw = "nopq", JoinDate = new DateTime(2018, 1, 20) },
+                ["rstu"] = new Acount() { Id = "rstu", Pw = "retu", JoinDate = new DateTime(2008, 5, 15) },
 
             };
-            var result = flowerDict.Remove("pansy2");
-
-            Console.WriteLine(result);
-            foreach (KeyValuePair<string, int> item in flowerDict)
+            Dictionary<string, Acount> compareDict = userDict.Where(p => DateTime.Compare(p.Value.JoinDate, new DateTime(2018, 1, 1)) == -1)
+                .ToDictionary(p => p.Key, p => p.Value);
+            foreach (var item in compareDict)
             {
-                Console.WriteLine("{0} == {1}", item.Key, item.Value);
+                Console.WriteLine(item.Value.JoinDate);
             }
 
-            var average = flowerDict.Average(p => p.Value);
-            Console.WriteLine(average);
-            int total = flowerDict.Sum(x => x.Value);
-            Console.WriteLine(  total);
-            var items = flowerDict.Where(x => x.Key.Length <= 5).ToDictionary(x => x.Key, x => x.Value).ToList();
-            items.ForEach(x => Console.WriteLine($"키는{x.Key} 값은{x.Value}"));
-
-            foreach (var key in flowerDict.Values)
-            {
-                Console.WriteLine(key);
-            }
 
         }
     }
