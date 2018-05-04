e1d017d7ded2e7866f3a7b18ee6a091644348cf0
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-05-04 17:34:04 +0900

    Hashset 사용법

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -6,46 +6,31 @@ using System.Threading.Tasks;
 
 namespace _7._1_Dictionary
 {
-    class MonthDay
+    class Acount
     {
-        public int Day { get; private set; }
-        public int Month { get; private set; }
-        public MonthDay(int month, int day)
-        {
-            this.Month = month;
-            this.Day = day;
-        }
-
-        public override bool Equals(object obj)
-        {
-            var day = obj as MonthDay;
-            return day != null &&
-                   Day == day.Day &&
-                   Month == day.Month;
-        }
-
-        public override int GetHashCode()
-        {
-            var hashCode = 93243338;
-            hashCode = hashCode * -1521134295 + Day.GetHashCode();
-            hashCode = hashCode * -1521134295 + Month.GetHashCode();
-            return hashCode;
-        }
+        public string Id { get; set; }
+        public string Pw { get; set; }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<MonthDay, string> dict = new Dictionary<MonthDay, string>
-            {
-                [new MonthDay(6, 6)] = "현충일",
-                [new MonthDay(8, 15)] = "광복절",
-                [new MonthDay(10, 3)] = "개천절",
+            HashSet<string> set = new HashSet<string>();
 
-            };
-            var md = new MonthDay(8, 15);
-            var s = dict[md];
-            Console.WriteLine(s);
+            set.Add("aa");
+            set.Add("aa");
+            set.Add("aa");
+            set.Add("aa");
+            set.Add("aa");
+            set.Add("aa");
+            set.Add("aab");
+
+
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
