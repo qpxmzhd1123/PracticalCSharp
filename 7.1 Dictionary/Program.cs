c93aadeea0987bd8d3cf264ca54273458bad7085
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-05-04 18:29:16 +0900

    Abbreviations 클래스를 이용한 예

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -52,7 +52,47 @@ namespace _7._1_Dictionary
     {
         static void Main(string[] args)
         {
-            
+            // 생성자를 호출한다.
+            var abbrs = new Abbrreviations();
+
+            // Add 메서드를 호출한 예
+            abbrs.Add("IOC", "국제 올림픽 위원회");
+            abbrs.Add("NPT", "핵확산방지조약");
+
+            // 인덱서를 사용한 예
+            var names = new[] { "WHO", "FIFA", "NPT" };
+            foreach (var name in names)
+            {
+                var fullname = abbrs[name];
+                if (fullname == null)
+                {
+                    Console.WriteLine("{0}을(를) 찾을 수 없습니다.", name);
+                }
+                else
+                {
+                    Console.WriteLine("{0}={1}", name, fullname);
+                }
+                Console.WriteLine();
+                // ToAbbreviation 메서드를 이용한 예
+                var korean = "동남아시아 국가 연합";
+                var abbreviation = abbrs.ToAbbreviation(korean);
+                if (abbreviation == null)
+                {
+                    Console.WriteLine("{0}을(를) 찾을 수 없습니다.", korean);
+                }
+                else
+                {
+                    Console.WriteLine("{0} 의 줄임말은 {1}입니다.", korean, abbreviation);
+                }
+                Console.WriteLine();
+
+                //FindAll 메서드를 이용한 예
+                foreach (var item in abbrs.FindAll("국제"))
+                {
+                    Console.WriteLine("{0}={1}", item.Key, item.Value);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
