c8275713ba905cdde28145ad331237dc721a2547
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-05-19 15:43:20 +0900

    out 키워드에 대해서

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -7,15 +7,26 @@ using System.Threading.Tasks;
 
 namespace _7._1_Dictionary
 {
-  
+    
     class Program
     {
         static void Main(string[] args)
         {
-            File.Delete(@"C:\Example\Greeting.txt");
+            
+            IsSumEven(out bool b,out int x, out int y, out int sum);
+            Console.WriteLine(b);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(sum);
         }
 
+        static void IsSumEven(out bool b, out int x, out int y, out int sum)
+        {
+            x = 5;
+            y = 3;
+            sum = x + y;
+            b = sum % 2 == 0;
 
-
+        }
     }
 }
