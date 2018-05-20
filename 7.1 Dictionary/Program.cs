ae90ffaf41c793be9e02fd5a8db0595bac7bd737
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-05-20 14:34:29 +0900

    예제10.11 Matches 메서드의 결과에 LINQ를 적용한다

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -14,11 +14,13 @@ namespace _7._1_Dictionary
     {
         static void Main(string[] args)
         {
-            var text = "private List<string> results = new List<stirng>();";
-            var matches = Regex.Matches(text, @"List<\w+>");
+            var text = "private List<string> results = new List<string>();";
+            var matches = Regex.Matches(text, @"\b[a-z]+\b")
+                               .Cast<Match>()
+                               .OrderBy(x => x.Length);
             foreach (Match match in matches)
             {
-                Console.WriteLine("Index={0}, Length={1}, Value={2}", match.Index, match.Length, match.Value);
+                Console.WriteLine("Index={0}, Length={1}, value={2}", match.Index, match.Length, match.Value);
             }
         }
 
