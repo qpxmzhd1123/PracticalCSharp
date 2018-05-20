1e61f8a437b9267b5275c5600131e1591eadc4ce
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-05-20 14:06:15 +0900

    예제10.6 지정한 패턴에 완전히 일치하는지 여부를 판정하는 나쁜 예

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -14,8 +14,8 @@ namespace _7._1_Dictionary
     {
         static void Main(string[] args)
         {
-            var strings = new[] { "Microsoft Window", "Window Server", "Windows", };
-            var regex = new Regex(@"^(W|w)indows$");
+            var strings = new[] { "Microsoft Windows", "Windows Server", "Windows", };
+            var regex = new Regex(@"(W|w)indows");//행의 시작과 끝 지점을 나타내는 특수 기호가 없으므로 모든 행에 일치함 
             var count = strings.Count(s => regex.IsMatch(s));
             Console.WriteLine("{0}행과 일치", count);
         }
