03da697773650dc3d80070fabaca7eb6aed1d164
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-05-20 15:07:58 +0900

    예제10.18 그룹화 기능을 이용한 치환(1)

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -14,9 +14,9 @@ namespace _7._1_Dictionary
     {
         static void Main(string[] args)
         {
-            var text = "foo.htm bar.html baz.htm";
-            var pattern = @"\.(htm)\b";
-            var replaced = Regex.Replace(text, pattern, ".html");
+            var text = "1024바이트,8바이트 문자,바이트,킬로바이트";
+            var pattern = @"(\d+)바이트";
+            var replaced = Regex.Replace(text, pattern, "$1byte");
             Console.WriteLine(replaced);
         }
 
