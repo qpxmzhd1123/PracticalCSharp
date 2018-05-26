1e77c11e7fc2829db5e977eb35c33dee90270e8c
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-05-26 21:14:46 +0900

    예제10.25 최단 일치의 수량자를 사용해 최단으로 일치시키는 예(1)

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -15,12 +15,13 @@ namespace _7._1_Dictionary
         static void Main(string[] args)
         {
             var text = "<person><name>김삿갓</name><age>22</age></person>";
-            var pattern = @"<.+>";
-            var matches = Regex.Matches(text, pattern);
-            foreach (Match m in matches)
-            {
-                Console.WriteLine("'{0}'", m.Value);
-            }
+            var pattern = @"<(.+?)>";
+            var matches = Regex.Match(text, pattern);
+            Console.WriteLine(matches.Groups[1].Value);
+            //foreach (Match m in matches)
+            //{
+            //    Console.WriteLine("'{0}'", m.Groups[1].Value);
+            //}
         }
 
     }
