8dbbb07c1c79fbef8e1a1cb5afe914b460f4a270
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-05-26 21:03:42 +0900

    예제10.22 수량자{n,m}을 사용한 예

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -14,9 +14,12 @@ namespace _7._1_Dictionary
     {
         static void Main(string[] args)
         {
-            var text = "a123456 b123 Z12345 AX98765";
-            var pattern = @"\b[a-zA-Z][0-9]{5,}\b";
+            var text = "삼겹살-84-58433,상추-95838-488,키보드-840-48484,마우스-3274-38,샴푸-489-58493,치약-38-4839,장갑-48490-483,통조림-3840-203,카레-43-28490,계란-48594-283";
+
+            var pattern = @"\p{IsHangulSyllables}+-[0-9]{2,3}-[0-9]+";
+
             var matches = Regex.Matches(text, pattern);
+
             foreach (Match m in matches)
             {
                 Console.WriteLine("'{0}'", m.Value);
