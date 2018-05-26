5b2461c75e4f63f5d029d0a58ee65d28388bc453
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-05-26 21:09:34 +0900

    예제10.23 최장 일치 원칙의 예

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -14,12 +14,9 @@ namespace _7._1_Dictionary
     {
         static void Main(string[] args)
         {
-            var text = "삼겹살-84-58433,상추-95838-488,키보드-840-48484,마우스-3274-38,샴푸-489-58493,치약-38-4839,장갑-48490-483,통조림-3840-203,카레-43-28490,계란-48594-283";
-
-            var pattern = @"\p{IsHangulSyllables}+-[0-9]{2,3}-[0-9]+";
-
+            var text = "<person><name>김삿갓</name><age>22</age></person>";
+            var pattern = @"<.+>";
             var matches = Regex.Matches(text, pattern);
-
             foreach (Match m in matches)
             {
                 Console.WriteLine("'{0}'", m.Value);
