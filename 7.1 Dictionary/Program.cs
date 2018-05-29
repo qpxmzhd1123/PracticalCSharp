74ba03680af8863656fb216a849e01fdd9ff2a1e
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-05-29 21:04:27 +0900

    문제10.3

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -14,17 +14,27 @@ namespace _7._1_Dictionary
     {
         static void Main(string[] args)
         {
-            var fai = @"C:\Example\Greeting.txt";
-            var pattern = @"(\w{3,})|(\d{3,})";
-            string[] lines = File.ReadAllLines(fai);
-            foreach (var line in lines)
+            var texts = new[] 
             {
-                var a = Regex.Matches(line, pattern);
-                foreach (var item in a)
+                "Time is money.",
+                "What time is it?",
+                "It will take time.",
+                "We reorganized the timetable",
+            };
+            var pattern = @"\btime\b";
+            foreach (var text in texts)
+            {
+                var mat = Regex.Match(text, pattern, RegexOptions.IgnoreCase);
+                if (mat.Success)
                 {
-                    Console.WriteLine(item);
+
+
+                    int index = mat.Index;
+                    string str = text;
+                    Console.WriteLine($"문자열은 {str} Time의 시작위치는{index}");
                 }
             }
+            
         }
 
     }
