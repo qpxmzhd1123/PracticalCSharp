eda86217b83c30f8e4b5138d63abe486d2e5bdae
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-06-06 07:42:03 +0900

    11.2.8 익명 클래스의 객체 형태로 요소를 구한다

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -15,11 +15,18 @@ namespace _7._1_Dictionary
         static void Main(string[] args)
         {
             var xdoc = XDocument.Load("novelists.xml");
-            var xtitles = xdoc.Root.Descendants("title");
-            foreach (var xtitle in xtitles)
+            var novelists = xdoc.Root.Elements()
+                                .Select(x => new
+                                {
+                                    Name = (string)x.Element("name"),
+                                    Birth = (DateTime)x.Element("birth"),
+                                    Death = (DateTime)x.Element("death")
+                                });
+            foreach (var novelist in novelists)
             {
-                Console.WriteLine(xtitle.Value);
+                Console.WriteLine("{0} ({1}-{2})", novelist.Name, novelist.Birth.Year, novelist.Death.Year);
             }
+
         }
     
     }
