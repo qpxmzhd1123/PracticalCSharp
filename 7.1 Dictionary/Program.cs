a6f3720a59357a75cc25bd02366ee4184a1ceb47
qpxmzhd1123 <qpxmzhd1123@gmail.com>
2018-06-06 08:02:15 +0900

    11.2.9 사용자 지정 클래스의 객체 형태로 요소를 구한다

--- 7.1 Dictionary/Program.cs
+++ 7.1 Dictionary/Program.cs
@@ -9,25 +9,45 @@ using System.Text.RegularExpressions;
 using System.Xml.Linq;
 
 namespace _7._1_Dictionary
-{
+{ 
+    class Novelist
+    {
+        public string Name { get; set; }
+        public string KanaName { get; set; }
+        public DateTime Birth { get; set; }
+        public DateTime Death { get; set; }
+        public IEnumerable<string> Masterpieces { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var xdoc = XDocument.Load("novelists.xml");
-            var novelists = xdoc.Root.Elements()
-                                .Select(x => new
-                                {
-                                    Name = (string)x.Element("name"),
-                                    Birth = (DateTime)x.Element("birth"),
-                                    Death = (DateTime)x.Element("death")
-                                });
+            var novelists = ReadNovelists();
             foreach (var novelist in novelists)
             {
-                Console.WriteLine("{0} ({1}-{2})", novelist.Name, novelist.Birth.Year, novelist.Death.Year);
+                Console.WriteLine("{0} ({1}-{2}) - {3}", 
+                    novelist.Name, novelist.Birth.Year, novelist.Death.Year, 
+                    string.Join(", ", novelist.Masterpieces));
             }
-
         }
-    
+
+        public static IEnumerable<Novelist> ReadNovelists()
+        {
+            var xdoc = XDocument.Load("novelists.xml");
+            var novelists = xdoc.Root.Elements()
+                                .Select(x => new Novelist
+                                {
+                                    Name = (string)x.Element("name"),
+                                    KanaName = (string)(x.Element("name").Attribute("kana")),
+                                    Birth = (DateTime)x.Element("birth"),
+                                    Death = (DateTime)x.Element("death"),
+                                    Masterpieces = x.Element("masterpieces")
+                                                    .Elements("title").Select(title => title.Value)
+                                                    .ToArray()
+
+                                });
+            return novelists.ToArray();
+        }
+
     }
 }
